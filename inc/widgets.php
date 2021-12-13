2a0168f3281f1d3b90a0035dca0724f826a48ca9
Aditya Kristyanto <42263468+aadiityaak@users.noreply.github.com>
2021-12-13 21:03:11 +0700

    Delete Function Hero

--- inc/widgets.php
+++ inc/widgets.php
@@ -124,42 +124,6 @@ if ( ! function_exists( 'sweetweb_widgets_init' ) ) {
 
 		register_sidebar(
 			array(
-				'name'          => __( 'Hero Slider', 'sweetweb' ),
-				'id'            => 'hero',
-				'description'   => __( 'Hero slider area. Place two or more widgets here and they will slide!', 'sweetweb' ),
-				'before_widget' => '<div class="carousel-item">',
-				'after_widget'  => '</div>',
-				'before_title'  => '',
-				'after_title'   => '',
-			)
-		);
-
-		register_sidebar(
-			array(
-				'name'          => __( 'Hero Canvas', 'sweetweb' ),
-				'id'            => 'herocanvas',
-				'description'   => __( 'Full size canvas hero area for Bootstrap and other custom HTML markup', 'sweetweb' ),
-				'before_widget' => '',
-				'after_widget'  => '',
-				'before_title'  => '',
-				'after_title'   => '',
-			)
-		);
-
-		register_sidebar(
-			array(
-				'name'          => __( 'Top Full', 'sweetweb' ),
-				'id'            => 'statichero',
-				'description'   => __( 'Full top widget with dynamic grid', 'sweetweb' ),
-				'before_widget' => '<div id="%1$s" class="static-hero-widget %2$s dynamic-classes">',
-				'after_widget'  => '</div><!-- .static-hero-widget -->',
-				'before_title'  => '<h3 class="widget-title">',
-				'after_title'   => '</h3>',
-			)
-		);
-
-		register_sidebar(
-			array(
 				'name'          => __( 'Footer Full', 'sweetweb' ),
 				'id'            => 'footerfull',
 				'description'   => __( 'Full sized footer widget with dynamic grid', 'sweetweb' ),
