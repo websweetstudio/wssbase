8bee85d10ff21612a2e631b16cdaafacce7ba3a9
aadiityaak <aadiityaak@gmail.com>
2022-07-25 14:17:44 +0700

    Fix sidebar in some woocommerce page

--- global-templates/left-sidebar-check.php
+++ global-templates/left-sidebar-check.php
@@ -11,7 +11,7 @@ defined( 'ABSPATH' ) || exit;
 $sidebar_pos = get_theme_mod( 'sweetweb_sidebar_position' );
 
 // disable sidebar in woocommerce pages
-if ( ('left' === $sidebar_pos || 'both' === $sidebar_pos) && !(class_exists( 'WooCommerce' ) && is_woocommerce()) ) {
+if ( ('left' === $sidebar_pos || 'both' === $sidebar_pos) && !(class_exists( 'WooCommerce' ) && (is_woocommerce() || is_cart() || is_checkout() || is_account_page())) ) {
 	get_template_part( 'sidebar-templates/sidebar', 'left' );
 }
 ?>
--- global-templates/right-sidebar-check.php
+++ global-templates/right-sidebar-check.php
@@ -14,6 +14,6 @@ defined( 'ABSPATH' ) || exit;
 <?php
 $sidebar_pos = get_theme_mod( 'sweetweb_sidebar_position' );
 // disable sidebar in woocommerce pages
-if ( ('right' === $sidebar_pos || 'both' === $sidebar_pos) && !(class_exists( 'WooCommerce' ) && is_woocommerce()) ) {
+if ( ('right' === $sidebar_pos || 'both' === $sidebar_pos) && !(class_exists( 'WooCommerce' ) && (is_woocommerce() || is_cart() || is_checkout() || is_account_page())) ) {
 	get_template_part( 'sidebar-templates/sidebar', 'right' );
 }
