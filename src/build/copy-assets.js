b34b5d6be5d952781d00349d45fa4d12d4056bb3
aadiityaak <aadiityaak@gmail.com>
2022-11-14 12:47:36 +0700

    remove glightbox js

--- src/build/copy-assets.js
+++ src/build/copy-assets.js
@@ -1,24 +1,24 @@
-const { promises: fs } = require("fs")
-const path = require("path")
+const { promises: fs } = require("fs");
+const path = require("path");
 
 async function copyDir(src, dest) {
-    await fs.mkdir(dest, { recursive: true });
-    let entries = await fs.readdir(src, { withFileTypes: true });
+	await fs.mkdir(dest, { recursive: true });
+	let entries = await fs.readdir(src, { withFileTypes: true });
 
-    for (let entry of entries) {
-        let srcPath = path.join(src, entry.name);
-        let destPath = path.join(dest, entry.name);
+	for (let entry of entries) {
+		let srcPath = path.join(src, entry.name);
+		let destPath = path.join(dest, entry.name);
 
-        entry.isDirectory() ?
-            await copyDir(srcPath, destPath) :
-            await fs.copyFile(srcPath, destPath);
-    }
+		entry.isDirectory()
+			? await copyDir(srcPath, destPath)
+			: await fs.copyFile(srcPath, destPath);
+	}
 }
 
 // Copy all Bootstrap SCSS files.
-copyDir('./node_modules/bootstrap/scss', './src/sass/assets/bootstrap');
+copyDir("./node_modules/bootstrap/scss", "./src/sass/assets/bootstrap");
 // Copy glightbox postcss directory
-copyDir('./node_modules/glightbox/src/postcss', './src/sass/assets/glightbox').then(() => {
-    // rename glightbox.css to glightbox.scss
-    fs.rename('./src/sass/assets/glightbox/glightbox.css', './src/sass/assets/glightbox/glightbox.scss');
-});
+// copyDir('./node_modules/glightbox/src/postcss', './src/sass/assets/glightbox').then(() => {
+//     // rename glightbox.css to glightbox.scss
+//     fs.rename('./src/sass/assets/glightbox/glightbox.css', './src/sass/assets/glightbox/glightbox.scss');
+// });
--- src/js/custom-javascript.js
+++
@@ -1,5 +0,0 @@
-// import glightbox from 'glightbox';
-import GLightbox from 'glightbox';
-const glightbox = new GLightbox({
-    selector: '.wp-block-gallery img',
-});
