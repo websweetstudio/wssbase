8199bd99d3aff99bfc7ebfc2f040243676c7706f
aadiityaak <aadiityaak@gmail.com>
2023-08-08 08:31:34 +0700

    add .vscode in ignore dist-build

--- src/build/dist-build.js
+++ src/build/dist-build.js
@@ -1,49 +1,49 @@
-const { promises: fs } = require("fs")
-const zipdir = require('zip-dir')
-const path = require("path")
-const del = require('del')
-
+const { promises: fs } = require("fs");
+const zipdir = require("zip-dir");
+const path = require("path");
+const del = require("del");
 
 async function copyDir(src, dest) {
-    await fs.mkdir(dest, { recursive: true });
-    let entries = await fs.readdir(src, { withFileTypes: true });
+	await fs.mkdir(dest, { recursive: true });
+	let entries = await fs.readdir(src, { withFileTypes: true });
 	let ignore = [
-		'node_modules',
-		'dist',
-		'src',
-		'.git',
-		'.github',
-		'.browserslistrc',
-		'.editorconfig',
-		'.gitattributes',
-		'.gitignore',
-		'.jscsrc',
-		'.jshintignore',
-		'.travis.yml',
-		'composer.json',
-		'composer.lock',
-		'package.json',
-		'package-lock.json',
-		'phpcs.xml.dist',
-		'.vscode'
+		"node_modules",
+		"dist",
+		"src",
+		".git",
+		".github",
+		".browserslistrc",
+		".editorconfig",
+		".gitattributes",
+		".gitignore",
+		".vscode",
+		".jscsrc",
+		".jshintignore",
+		".travis.yml",
+		"composer.json",
+		"composer.lock",
+		"package.json",
+		"package-lock.json",
+		"phpcs.xml.dist",
+		".vscode",
 	];
 
-    for (let entry of entries) {
-		if ( ignore.indexOf( entry.name ) != -1 ) {
+	for (let entry of entries) {
+		if (ignore.indexOf(entry.name) != -1) {
 			continue;
 		}
-        let srcPath = path.join(src, entry.name);
-        let destPath = path.join(dest, entry.name);
+		let srcPath = path.join(src, entry.name);
+		let destPath = path.join(dest, entry.name);
 
-        entry.isDirectory() ?
-            await copyDir(srcPath, destPath) :
-            await fs.copyFile(srcPath, destPath);
-    }
+		entry.isDirectory()
+			? await copyDir(srcPath, destPath)
+			: await fs.copyFile(srcPath, destPath);
+	}
 }
-del('./dist').then (() => {
-	console.log('./dist is deleted!');
-	copyDir('./', './dist/sweetweb/sweetweb').then (() => {
-		zipdir('./dist/sweetweb', { saveTo: './dist/sweetweb.zip' });
-		console.log('Zip file created');
+del("./dist").then(() => {
+	console.log("./dist is deleted!");
+	copyDir("./", "./dist/sweetweb/sweetweb").then(() => {
+		zipdir("./dist/sweetweb", { saveTo: "./dist/sweetweb.zip" });
+		console.log("Zip file created");
 	});
 });
